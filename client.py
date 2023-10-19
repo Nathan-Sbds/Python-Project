d07945c2200001487013c26c116d65f91e8825c7
Nathan-Sbds <118532002+Nathan-Sbds@users.noreply.github.com>
2023-10-19 12:27:43 +0200

    test file and adding pytest

--- client.py
+++ client.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
-import requests,json
+import requests
+import json
 
+# Dictionary of API functions
 functions = {
     "Search Artists by Name": "/artist/param_to_set/name",
     "Albums by Artist ID": "/artist/param_to_set/albums",
@@ -13,72 +15,118 @@ functions = {
     "Tracks by Customer ID": "/customer/param_to_set/tracks"
 }
 
+# Function to make API requests
 def make_api_request():
+    """
+    Make a request to the API based on the selected function and parameter.
+    
+    Parameters:
+    None
+    
+    Returns:
+    None
+    """
+    # Get the selected function and parameter
     selected_function = function_combobox.get()
     param = parameter_entry.get()
-    
+
+    # Construct the API endpoint based on the selected function and parameter
     if selected_function in functions:
-        api_endpoint = "http://127.0.0.1:8000" +functions[selected_function].replace("param_to_set",param)
-        if selected_function in ["Tracks by Album ID", "Tracks by Playlist ID", "Tracks by Artist ID","Tracks by Genre ID","Tracks by Customer ID"] and shuffle_var.get()==1:
+        api_endpoint = "http://127.0.0.1:8000" + functions[selected_function].replace("param_to_set", param)
+        if selected_function in ["Tracks by Album ID", "Tracks by Playlist ID", "Tracks by Artist ID", "Tracks by Genre ID", "Tracks by Customer ID"] and shuffle_var.get() == 1:
             api_endpoint += "/shuffle"
-        
+
+        # Print the API endpoint for debugging purposes
         print(api_endpoint)
+
+        # Make a GET request to the API endpoint
         response = requests.get(api_endpoint)
+
+        # If the request is successful, display the data, otherwise display an error message
         if response.status_code == 200:
             data = response.json()
             display_data(data)
         else:
             display_data({"Error": f"Request failed with status code: {response.status_code}"})
 
+# Function to display API response data
 def display_data(data):
+    """
+    Format and display the API response data.
+    
+    Parameters:
+    data (dict): The data retrieved from the API.
+    
+    Returns:
+    None
+    """
+    # Format the data to display it in the text field
     formatted_data = json.dumps(data, indent=2)
-
     lines = formatted_data.split('\n')
-
     clean_lines = [line for line in lines if all(char not in line for char in ['{', '}', ']', '    ['])]
-
-    clean_data = '\n'.join(clean_lines).replace(',',"").replace('[',"").replace('"',"").replace("£","£\n")
+    clean_data = '\n'.join(clean_lines).replace(',', "").replace('[', "").replace('"', "").replace("£", "£\n")
     clean_data = bytes(clean_data, "utf-8").decode("unicode_escape")
+
+    # Update the text field with the formatted data
     result_text.configure(state="normal")
     result_text.delete(1.0, tk.END)
     result_text.insert(tk.END, clean_data)
     result_text.configure(state="disabled")
 
+# Function triggered when a function is selected
 def on_function_selection(event):
+    """
+    Show or hide the shuffle checkbox based on the selected function.
+    
+    Parameters:
+    event (Event): The event triggered by function selection.
+    
+    Returns:
+    None
+    """
+    # Show the shuffle checkbox if the selected function requires it, otherwise hide it
     selected_function = function_combobox.get()
-    if selected_function in ["Tracks by Album ID", "Tracks by Playlist ID", "Tracks by Artist ID","Tracks by Genre ID","Tracks by Customer ID"]:
+    if selected_function in ["Tracks by Album ID", "Tracks by Playlist ID", "Tracks by Artist ID", "Tracks by Genre ID", "Tracks by Customer ID"]:
         shuffle_checkbox.grid(row=1, column=2, columnspan=2)
     else:
         shuffle_checkbox.grid_forget()
 
-
+# Create the main window
 root = tk.Tk()
 root.title("Music Player")
 root.iconbitmap("player_icon.ico")
 
+# Create the function selection combobox
 function_combobox = ttk.Combobox(root, values=list(functions.keys()))
 function_combobox.grid(row=0, column=1, sticky="ew")
 function_combobox.bind("<<ComboboxSelected>>", on_function_selection)
 
+# Create the parameter entry field
 parameter_entry = tk.Entry(root)
 parameter_entry.grid(row=1, column=1, columnspan=2, sticky="ew")
 
+# Create the parameter label
 parameter_label = tk.Label(root, text="Parameter:")
 parameter_label.grid(row=1, column=0)
 
+# Create the shuffle variable
 shuffle_var = tk.IntVar()
 
+# Create the shuffle checkbox
 shuffle_checkbox = tk.Checkbutton(root, text="Shuffle", variable=shuffle_var)
 shuffle_checkbox.grid(row=1, column=2)
 shuffle_checkbox.grid_forget()
 
+# Create the request button
 request_button = tk.Button(root, text="Send the request", command=make_api_request)
 request_button.grid(row=2, column=0, columnspan=2)
 
+# Create the result text field
 result_text = tk.Text(root)
 result_text.configure(state="disabled")
 result_text.grid(row=3, column=0, columnspan=3, sticky="nsew")
 
+# Create the scrollbar for the result text field
 scrollbar = tk.Scrollbar(root, command=result_text.yview)
 scrollbar.grid(row=3, column=3, sticky='ns')
 
@@ -87,4 +135,5 @@ result_text.config(yscrollcommand=scrollbar.set)
 root.grid_rowconfigure(3, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
+# Run the main event loop
 root.mainloop()
--- server/api.py
+++ server/api.py
@@ -1,146 +1,217 @@
+# Import the necessary modules
 import random
-from fastapi import FastAPI, Path,Depends
-from database import *
+from fastapi import FastAPI, Path, Depends, HTTPException
+from server.database import *
 
+# Initialize the FastAPI app
 app = FastAPI()
 
+# Define a function to get the database
 def get_db():
-    db =SessionLocal()
+    """
+    Function to get the database connection.
+    """
+    db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
 
-
+# Endpoint for searching artists by name
 @app.get("/artist/{artist_name:path}/name")
-async def search_artists_by_name(artist_name: str = Path(..., description="Artist Name"), db = Depends(get_db)):
+async def search_artists_by_name(artist_name: str = Path(..., description="Artist Name"), db=Depends(get_db)):
+    """
+    Endpoint to search for artists by name.
+    """
+    # Fetch artists from the database based on the provided artist name
     artists = await get_artists_by_name(db, artist_name)
     if artists == []:
-        return "Artist Not Found"
+        raise HTTPException(status_code=404, detail="Artist Not Found")
+    # Format the retrieved artist data and return it
     artist_data = [{"Name": artist.Name, "ArtistId": artist.ArtistId} for artist in artists]
     return artist_data
-    
+
+# Endpoint for retrieving albums by artist ID
 @app.get("/artist/{artist_id}/albums")
-async def albums_by_artist_id(artist_id: int = Path(..., description="artist ID"),db = Depends(get_db)):
+async def albums_by_artist_id(artist_id: int = Path(..., description="artist ID"), db=Depends(get_db)):
+    """
+    Endpoint to retrieve albums by artist ID.
+    """
+    # Fetch albums from the database based on the provided artist ID
     albums = await get_album_by_artist_id(db, artist_id)
-    print(albums)
     if albums == []:
-        return "Artist Not Found"
+        raise HTTPException(status_code=404, detail="Artist Not Found")
+    # Format the retrieved album data and return it
     album_data = [{"Title": album.Title} for album in albums]
     return album_data
 
+# Endpoint for retrieving tracks by album ID
 @app.get("/albums/{album_id}/tracks")
-async def tracks_by_album_id(album_id: int = Path(..., description="album ID"),db = Depends(get_db)):
+async def tracks_by_album_id(album_id: int = Path(..., description="album ID"), db=Depends(get_db)):
+    """
+    Endpoint to retrieve tracks by album ID.
+    """
+    # Fetch tracks from the database based on the provided album ID
     tracks = await get_track_names_by_album_id(db, album_id)
     if tracks == []:
-        return "Album Not Found"
-    track_data = [{"Name": (f"{track.TrackId}: {track.Name}",f"  {str(round(track.UnitPrice, 2))} £")} for track in tracks]
+        raise HTTPException(status_code=404, detail="Album Not Found")
+    # Format the retrieved track data and return it
+    track_data = [{"Name": (f"{track.TrackId}: {track.Name}", f"  {str(round(track.UnitPrice, 2))} £")} for track in tracks]
     return track_data
 
-
+# Endpoint for shuffling tracks by album ID
 @app.get("/albums/{album_id}/tracks/shuffle")
-async def tracks_by_album_id(album_id: int = Path(..., description="album ID"),db = Depends(get_db)):
+async def tracks_by_album_id_shuffle(album_id: int = Path(..., description="album ID"), db=Depends(get_db)):
+    """
+    Endpoint to shuffle tracks by album ID.
+    """
+    # Fetch tracks from the database based on the provided album ID
     tracks = await get_track_names_by_album_id(db, album_id)
     if tracks == []:
-        return "Album Not Found"
+        raise HTTPException(status_code=404, detail="Album Not Found")
+    # Shuffle the retrieved tracks and format the data before returning it
     random.shuffle(tracks)
-    track_data = [{"Name": (f"{track.TrackId}: {track.Name}",f"  {str(round(track.UnitPrice, 2))} £")} for track in tracks]
+    track_data = [{"Name": (f"{track.TrackId}: {track.Name}", f"  {str(round(track.UnitPrice, 2))} £")} for track in tracks]
     return track_data
-
-
+# Endpoint for retrieving an artist with their albums and tracks
 @app.get("/artist/{artist_id}")
-async def artist_with_albums_and_tracks(artist_id: int = Path(..., description="artist ID"),db = Depends(get_db)):
+async def artist_with_albums_and_tracks(artist_id: int = Path(..., description="artist ID"), db=Depends(get_db)):
+    """
+    Endpoint to retrieve an artist along with their albums and tracks.
+    """
+    # Retrieve the artist based on the provided artist ID
     artist = await get_artists_by_id(db, artist_id)
-    if artist == None:
-        return "Artist Not Found"
+    if artist is None:
+        raise HTTPException(status_code=404, detail="Artist Not Found")
+    # Prepare the data structure for the artist and their albums
     result = {}
-
     result["Artist"] = artist.Name
     albums = await get_album_by_artist_id(db, artist.ArtistId)
     albums_data = []
-
+    # Retrieve tracks for each album and format the data
     for album in albums:
         album_data = {"Album": album.Title}
         tracks = await get_track_names_by_album_id(db, album.AlbumId)
-        track_names = [(f"{track.TrackId}: {track.Name}",f"  {str(round(track.UnitPrice, 2))} £") for track in tracks]
+        track_names = [(f"{track.TrackId}: {track.Name}", f"  {str(round(track.UnitPrice, 2))} £") for track in tracks]
         album_data["Songs"] = track_names
         albums_data.append(album_data)
-
     result["Albums"] = albums_data
     return result
 
+# Endpoint for retrieving tracks by playlist ID
 @app.get("/playlist/{playlist_id}")
-async def tracks_by_playlist_id(playlist_id: int = Path(..., description="playlist ID"),db = Depends(get_db)):
+async def tracks_by_playlist_id(playlist_id: int = Path(..., description="playlist ID"), db=Depends(get_db)):
+    """
+    Endpoint to retrieve tracks by playlist ID.
+    """
+    # Retrieve tracks based on the provided playlist ID
     tracks = await get_track_by_playlist_id(db, playlist_id)
     playlist = await get_playlist_by_playlist_id(db, playlist_id)
-    if playlist == None:
-        return "Playlist Not Found"
-    return {"Playlist": f"{playlist.PlaylistId}: {playlist.Name}", "Tracks": [(f"{track.TrackId}: {track.Name}",f"  {str(round(track.UnitPrice, 2))} £") for track in tracks]}
+    if playlist is None:
+        raise HTTPException(status_code=404, detail="Playlist Not Found")
+    # Format and return the playlist and its associated tracks
+    return {"Playlist": f"{playlist.PlaylistId}: {playlist.Name}", "Tracks": [(f"{track.TrackId}: {track.Name}", f"  {str(round(track.UnitPrice, 2))} £") for track in tracks]}
 
+# Endpoint for shuffling tracks by playlist ID
 @app.get("/playlist/{playlist_id}/shuffle")
-async def tracks_by_playlist_id_shuffle(playlist_id: int = Path(..., description="playlist ID"),db = Depends(get_db)):
+async def tracks_by_playlist_id_shuffle(playlist_id: int = Path(..., description="playlist ID"), db=Depends(get_db)):
+    """
+    Endpoint to shuffle tracks by playlist ID.
+    """
+    # Retrieve tracks based on the provided playlist ID
     tracks = await get_track_by_playlist_id(db, playlist_id)
     random.shuffle(tracks)
     num_tracks_to_return = min(20, len(tracks))
     random_tracks = tracks[:num_tracks_to_return]
     playlist = await get_playlist_by_playlist_id(db, playlist_id)
-    if playlist == None:
-        return "Playlist Not Found"
-    return {"Playlist": f"{playlist.PlaylistId}: {playlist.Name}", "Tracks": [(f"{track.TrackId}: {track.Name}",f"  {str(round(track.UnitPrice, 2))} £") for track in random_tracks]}
+    if playlist is None:
+        raise HTTPException(status_code=404, detail="Playlist Not Found")
+    # Format and return the shuffled playlist tracks
+    return {"Playlist": f"{playlist.PlaylistId}: {playlist.Name}", "Tracks": [(f"{track.TrackId}: {track.Name}", f"  {str(round(track.UnitPrice, 2))} £") for track in random_tracks]}
 
-
+# Endpoint for retrieving tracks by artist ID
 @app.get("/artist/{artist_id}/tracks")
-async def tracks_by_artist_id(artist_id: int = Path(..., description="artist ID"),db = Depends(get_db)):
+async def tracks_by_artist_id(artist_id: int = Path(..., description="artist ID"), db=Depends(get_db)):
+    """
+    Endpoint to retrieve tracks by artist ID.
+    """
+    # Retrieve tracks based on the provided artist ID
     tracks = await get_track_by_artist_id(db, artist_id)
     artist = await get_artists_by_id(db, artist_id)
-    if artist == None:
-        return "Artist Not Found"
-    return {"Artist": f"{artist.ArtistId}: {artist.Name}", "Tracks": [(f"{track.TrackId}: {track.Name}",f"  {str(round(track.UnitPrice, 2))} £") for track in tracks]}
+    if artist is None:
+        raise HTTPException(status_code=404, detail="Artist Not Found")
+    # Format and return the artist ID along with their tracks
+    return {"Artist": f"{artist.ArtistId}: {artist.Name}", "Tracks": [(f"{track.TrackId}: {track.Name}", f"  {str(round(track.UnitPrice, 2))} £") for track in tracks]}
 
-
+# Endpoint for shuffling tracks by atrist ID
 @app.get("/artist/{artist_id}/tracks/shuffle")
-async def tracks_by_artist_id_shuffle(artist_id: int = Path(..., description="artist ID"),db = Depends(get_db)):
+async def tracks_by_artist_id_shuffle(artist_id: int = Path(..., description="artist ID"), db=Depends(get_db)):
+    """
+    Endpoint to shuffle tracks by artist ID.
+    """
+    # Retrieve tracks based on the provided artist ID
     tracks = await get_track_by_artist_id(db, artist_id)
     random.shuffle(tracks)
     artist = await get_artists_by_id(db, artist_id)
-    if artist == None:
-        return "Artist Not Found"
-    return {"Artist": f"{artist.ArtistId}: {artist.Name}", "Tracks": [(f"{track.TrackId}: {track.Name}",f"  {str(round(track.UnitPrice, 2))} £") for track in tracks]}
+    if artist is None:
+        raise HTTPException(status_code=404, detail="Artist Not Found")
+    # Format and return the shuffled tracks associated with the artist
+    return {"Artist": f"{artist.ArtistId}: {artist.Name}", "Tracks": [(f"{track.TrackId}: {track.Name}", f"  {str(round(track.UnitPrice, 2))} £") for track in tracks]}
 
-
+# Endpoint for retrieving genre by album ID
 @app.get("/genre/{genre_id}/tracks")
-async def tracks_by_genre_id(genre_id: int = Path(..., description="genre ID"),db = Depends(get_db)):
+async def tracks_by_genre_id(genre_id: int = Path(..., description="genre ID"), db=Depends(get_db)):
+    """
+    Endpoint to retrieve tracks by genre ID.
+    """
+    # Retrieve tracks based on the provided genre ID
     tracks = await get_track_by_genre_id(db, genre_id)
     genre = await get_genre_by_id(db, genre_id)
-    if genre == None:
-        return "Genre Not Found"
-    return {"Genre": f"{genre.GenreId}: {genre.Name}", "Tracks": [(f"{track.TrackId}: {track.Name}",f"  {str(round(track.UnitPrice, 2))} £") for track in tracks]}
+    if genre is None:
+        raise HTTPException(status_code=404, detail="Genre Not Found")
+    # Format and return the genre along with its associated tracks
+    return {"Genre": f"{genre.GenreId}: {genre.Name}", "Tracks": [(f"{track.TrackId}: {track.Name}", f"  {str(round(track.UnitPrice, 2))} £") for track in tracks]}
 
-
+# Endpoint for shuffling tracks by genre ID
 @app.get("/genre/{genre_id}/tracks/shuffle")
-async def tracks_by_genre_id_shuffle(genre_id: int = Path(..., description="genre ID"),db = Depends(get_db)):
+async def tracks_by_genre_id_shuffle(genre_id: int = Path(..., description="genre ID"), db=Depends(get_db)):
+    """
+    Endpoint to shuffle tracks by genre ID.
+    """
+    # Retrieve tracks based on the provided genre ID
     tracks = await get_track_by_genre_id(db, genre_id)
     random.shuffle(tracks)
     genre = await get_genre_by_id(db, genre_id)
-    if genre == None:
-        return "Genre Not Found"
-    return {"Genre": f"{genre.GenreId}: {genre.Name}", "Tracks": [(f"{track.TrackId}: {track.Name}",f"  {str(round(track.UnitPrice, 2))} £") for track in tracks]}
+    if genre is None:
+        raise HTTPException(status_code=404, detail="Genre Not Found")
+    # Format and return the shuffled tracks associated with the genre
+    return {"Genre": f"{genre.GenreId}: {genre.Name}", "Tracks": [(f"{track.TrackId}: {track.Name}", f"  {str(round(track.UnitPrice, 2))} £") for track in tracks]}
 
-
+# Endpoint for retrieving tracks by customer ID
 @app.get("/customer/{customer_id}/tracks")
-async def tracks_by_customer_id(customer_id: int = Path(..., description="customer ID"),db = Depends(get_db)):
+async def tracks_by_customer_id(customer_id: int = Path(..., description="customer ID"), db=Depends(get_db)):
+    """
+    Endpoint to retrieve tracks by customer ID.
+    """
+    # Retrieve tracks based on the provided customer ID
     tracks = await get_track_by_customer_id(db, customer_id)
     customer = await get_customer_by_id(db, customer_id)
-    if customer == None:
-        return "Customer Not Found"
-    return {"Customer": [(f"{customer.CustomerId}: {customer.FirstName} {customer.LastName}")], "Tracks": [(f"{track.TrackId}: {track.Name}",f"  {str(round(track.UnitPrice, 2))} £") for track in tracks]}
+    if customer is None:
+        raise HTTPException(status_code=404, detail="Customer Not Found")
+    # Format and return the customer along with their associated tracks
+    return {"Customer": [(f"{customer.CustomerId}: {customer.FirstName} {customer.LastName}")], "Tracks": [(f"{track.TrackId}: {track.Name}", f"  {str(round(track.UnitPrice, 2))} £") for track in tracks]}
 
-
+# Endpoint for shuffling tracks by customer ID
 @app.get("/customer/{customer_id}/tracks/shuffle")
-async def tracks_by_customer_id(customer_id: int = Path(..., description="customer ID"),db = Depends(get_db)):
+async def tracks_by_customer_id(customer_id: int = Path(..., description="customer ID"), db=Depends(get_db)):
+    """
+    Endpoint to shuffle tracks by customer ID.
+    """
+    # Retrieve tracks based on the provided customer ID
     tracks = await get_track_by_customer_id(db, customer_id)
     random.shuffle(tracks)
     customer = await get_customer_by_id(db, customer_id)
-    if customer == None:
-        return "Customer Not Found"
-    return {"Customer": [(f"{customer.CustomerId}: {customer.FirstName} {customer.LastName}")], "Tracks": [(f"{track.TrackId}: {track.Name}",f"  {str(round(track.UnitPrice, 2))} £") for track in tracks]}
+    if customer is None:
+        raise HTTPException(status_code=404, detail="Customer Not Found")
+    # Format and return the shuffled tracks associated with the customer
+    return {"Customer": [(f"{customer.CustomerId}: {customer.FirstName} {customer.LastName}")], "Tracks": [(f"{track.TrackId}: {track.Name}", f"  {str(round(track.UnitPrice, 2))} £") for track in tracks]}
